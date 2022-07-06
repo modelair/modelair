e836757eba6506870eb3ed4401612866b05ebfc6
modelair <zetsoft@mail.ru>
2022-07-06 17:37:55 +1200

    1.0.6

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-module.exports = require('./dist/bundle')
-module.exports.fields = require('./dist/bundle').fields
-module.exports.patterns = require('./dist/bundle').patterns
+export * from './src/modelair'
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  // entry: {
+  //   app: './src/index.js',
+  //   print: './src/print.js'
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+}
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   sourceMap: true,
+    //   compress: {
+    //     warnings: false
+    //   }
+    // }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
